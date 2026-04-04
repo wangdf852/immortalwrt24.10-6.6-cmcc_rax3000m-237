54f80eea5495f79d5535408750ea31d1f79fa460
wangdf852 <79922028+wangdf852@users.noreply.github.com>
2026-04-04 23:30:05 +0800

    diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,7 +17,7 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 sed -i 's/OpenWrt/NbWrt-RAX3000M/g' package/base-files/files/bin/config_generate
 
 # 3. 安装依赖（OpenClash必需，保留）
-sudo apt install libfuse-dev
+#sudo apt install libfuse-dev
 
 # 4. 更新Go语言（OpenClash必需，保留）
 rm -rf feeds/packages/lang/golang
